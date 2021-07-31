05d2a646fc1f50f12cfc9730590bc12300c690a5
fuat <fuat@binnaz.com>
2021-08-01 02:08:13 +0300

    user add route added

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 const env = require('dotenv').config();
-const {User} = require('../models');
+const {User, Case} = require('../models');
 
 /* GET users listing. */
 router.post('/',  function (req, res, next) {
 
 });
 
-router.post('/addUser', async function (req, res, next) {
+router.post('/add', async function (req, res, next) {
     // res.send('user route');
-    const {name,email} = req.body.params;
-    console.log(name,email);
+    const {name,email,password,company} = req.body;
+
     try{
-        const user =  await User.create({name,email})
+        const userExists = await User.findOne({
+            where: {
+                email: email
+            },
+        });
+
+        if(userExists){
+            res.send('User already exists!');
+            return;
+        }
+
+    }catch (err){
+        console.log(err);
+    }
+
+    try{
+        const user =  await User.create({name,email,password,company})
         res.send(user);
     }catch (err){
         console.log(err);
     }
+
 });
 
 module.exports = router;
